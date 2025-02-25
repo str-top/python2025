2afaf9c161f14e3f56cda785cd2e6255c55bae53
IvanovaAdel <adelinaivanova13@mail.ru>
2025-02-25 19:33:04 +0500

    if.py
    Код с условием

---
+++ students/Ivanova-A/if.py
@@ -0,0 +1,9 @@
+chislo = int(input('Введите число'))
+if chislo > 10:
+  print('Ваше число больше десяти')
+elif chislo > 5:
+  print('Ваше число больше пяти')
+elif chislo > 0:
+  print('Ваше число больше нуля')
+else:
+  print('Число отрицательное')
