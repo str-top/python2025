feb46a6a76fe83862b48bc8a3522c9e6703ce560
adelinakim <adelin4ik91@gmail.com>
2025-04-29 21:25:20 +0500

    tuple43.py

---
+++ students/IshmuhametovaA/tuple43.py
@@ -0,0 +1,6 @@
+with open('input.txt', 'r') as a:
+    with open('numbers.txt', 'w') as f:
+        for line in a:
+            if any(char.isdigit() for char in line):
+                f.write(line)
+print(a.read())
