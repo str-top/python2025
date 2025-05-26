c0d90fb9d262843402b39bb2820e17bdfa22af80
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-05-26 21:20:32 +0500

    HW zoo.py

---
+++ students/Krasilnikov B/zoo.py
@@ -0,0 +1,49 @@
+class Animal:
+    def __init__(self,name,species,age,sound):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.sound = sound
+    
+    def make_sound(self):
+        print(f'{self.species} говорит {self.sound}!')
+        
+    
+    def info(self):
+        print(f'''
+        Кличка:{self.name}
+        Вид:{self.species}
+        Возраст:{self.age}
+        ''')
+        
+
+
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+    
+    def add_animal(self,animal):
+        self.animals.append(animal)
+        
+    def show_animals(self):
+        for animal in self.animals:
+            animal.info()
+    def make_all_sounds(self):
+        for animal in self.animals:
+            animal.make_sound()
+            
+lion = Animal('barsik','lion',3,'arrrrw')
+
+# lion.make_sound()
+# lion.info()
+
+cat = Animal('Almas','cat',6,'mew')
+dog = Animal('Omela','dog',4,'gav')
+zoo = Zoo()
+
+zoo.add_animal(lion)
+zoo.add_animal(dog)
+zoo.add_animal(cat)
+zoo.show_animals()
+zoo.make_all_sounds()
