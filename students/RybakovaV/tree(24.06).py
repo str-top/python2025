5c953f017252c80a218fcd04784309e1ade401e6
Валерия <valerikr.3005@gmail.com>
2025-07-05 21:18:11 +0500

    tree(24.06).py

---
+++ students/RybakovaV/tree(24.06).py
@@ -0,0 +1,46 @@
+class Observer:
+    def update(self, temperature):
+        raise NotImplementedError("Метод update должен быть переопределен")
+
+class WeatherStation:
+    def __init__(self):
+        self._observers = []
+        self._temperature = None
+
+    def add_observer(self, observer):
+        self._observers.append(observer)
+
+    def remove_observer(self, observer):
+        self._observers.remove(observer)
+
+    def set_temperature(self, temperature):
+        print(f"\n[WeatherStation] Новая температура: {temperature}°C")
+        self._temperature = temperature
+        self.notify_observers()
+
+    def notify_observers(self):
+        for observer in self._observers:
+            observer.update(self._temperature)
+
+class PhoneDisplay(Observer):
+    def update(self, temperature):
+        print(f"[PhoneDisplay] Новая температура: {temperature}°C")
+
+class WebDisplay(Observer):
+    def update(self, temperature):
+        print(f"[WebDisplay] Новая температура: {temperature}°C")
+
+
+station = WeatherStation()
+
+phone = PhoneDisplay()
+web = WebDisplay()
+
+station.add_observer(phone)
+station.add_observer(web)
+
+station.set_temperature(22)
+station.set_temperature(25)
+
+station.remove_observer(phone)
+station.set_temperature(28)
